dbfb7f5008a6784d8f4b95de845efb4b53ac160c
IcodeAlpha <alphacode4@outlook.com>
2024-06-07 04:15:06 +0300

    save 2

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import CountUp from 'react-countup/build/CountUp'
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+        <CountUp
+            decimals={2}
+            decimal=","
+            prefix="$"
+            end={amount} 
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
